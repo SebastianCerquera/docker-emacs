5e2f00576f9fcfafc3bdea90d631a8dfa21ab685
Sebastian Gonzalez <sebastian2zen@gmail.com>
2020-01-06 20:12:39 -0500

    sets proper task state

--- lain/lain.el
+++ lain/lain.el
@@ -116,10 +116,10 @@
   (org-todo 'right)
   ;; this is suppsed to be executed as a hook but it is not running when the command is invoked non interactively.
 ;  (set-marker org-log-note-marker (point))   
-;  (org-add-log-note)
+  (org-add-log-note)
   (org-narrow-to-subtree)
   (switch-to-buffer (current-buffer))
-  (org-log-note-update "DONE" date time
+  (org-log-note-update state date time
                        ;; the if evaluates true executed either the field is empty or not
                        (if link
                            (if (not (string-empty-p  (or link "")))
